8c2a0535e900b8a5497b1597e30a3162eefa8440
Promise Uzor <uzorpromiseokwudiri@gmail.com>
2024-12-02 01:01:07 +0000

    "Updated styles for chat box content, including timer, chat bubbles, and image container"

--- frontend/src/components/layouts/ChatBoxContent.jsx
+++ frontend/src/components/layouts/ChatBoxContent.jsx
@@ -5,11 +5,11 @@ const ChatContent = () => {
     <div className="p-4 flex-grow flex flex-col gap-4 overflow-y-auto">
       {/* Timer */}
       <div className="flex items-center text-gray-400">
-        <div className="flex-1 h-px bg-gray-300"></div>
+        <div className="flex-1 h-px bg-gray-300 dark:bg-gray-600"></div>
         <span className="px-3 py-1 text-xs border font-semibold rounded-full uppercase">
           Monday
         </span>
-        <div className="flex-1 h-px bg-gray-300"></div>
+        <div className="flex-1 h-px bg-gray-300 dark:bg-gray-600"></div>
       </div>
 
       {/* Chats */}
@@ -20,7 +20,7 @@ const ChatContent = () => {
             11:03 AM
             <Clock className="size-3" />
           </span>
-          <div className="bg-[#e5f0ff] border border-gray-200 shadow rounded-xl rounded-br-none text-sm flex flex-col max-w-[400px] md:max-w-[500px] relative">
+          <div className="bg-blue-600 border rounded-xl rounded-br-none text-sm flex flex-col max-w-[400px] md:max-w-[500px] relative text-white">
             <p className="p-2">
               Hi, Jane, I&apos;m Looking forward to meeting You Lorem ipsum
               dolor sit amet consectetur adipisicing elit. Cupiditate, libero
@@ -39,7 +39,7 @@ const ChatContent = () => {
             11:23 AM
             <Clock className="size-3" />
           </span>
-          <div className="bg-[#fafafb] border border-gray-200 shadow text-gray-900 rounded-xl rounded-bl-none text-sm items-start max-w-[400px] md:max-w-[500px]">
+          <div className="bg-blue-50 border text-gray-900 rounded-xl rounded-bl-none text-sm items-start max-w-[400px] md:max-w-[500px]">
             <p className="p-2">
               Hi, Jane, I&apos;m Looking forward to meeting You Lorem ipsum
               dolor sit amet consectetur adipisicing elit. Cupiditate, libero
@@ -55,7 +55,7 @@ const ChatContent = () => {
             11:40 AM
             <Clock className="size-3" />
           </span>
-          <div className="bg-[#e5f0ff] border shadow border-gray-200 rounded-xl rounded-br-none text-sm flex items-end flex-col max-w-[400px] md:max-w-[500px] relative">
+          <div className="bg-blue-600 text-white border rounded-xl rounded-br-none text-sm flex items-end flex-col max-w-[400px] md:max-w-[500px] relative">
             <img
               src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDg0fHx1c2VyfGVufDB8fHx8MTczMjU1NjgyMHww&ixlib=rb-4.0.3&q=80&w=1080"
               alt="sent Image"
@@ -76,7 +76,7 @@ const ChatContent = () => {
             11:23 AM
             <Clock className="size-3" />
           </span>
-          <div className="bg-[#fafafb] border border-gray-200 shadow rounded-xl rounded-bl-none text-sm items-start max-w-[400px] md:max-w-[500px]">
+          <div className="bg-blue-50 border text-gray-900 rounded-xl rounded-bl-none text-sm items-start max-w-[400px] md:max-w-[500px]">
             <a
               href="https://example.com"
               className="text-blue-600 font-semibold underline p-2 hover:blur-none blur-sm inline-block break-words"
